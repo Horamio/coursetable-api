6a6ec3d995cce3cf4c0d8001a50e0aba40934ac9
Ricardo Yrupailla <ry.yrupailla@gmail.com>
2020-10-25 08:29:05 -0500

    Add filter to courses index

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,6 +1,13 @@
 class CoursesController < ApplicationController
   def index
-    render json: Course.all
+    courses = Course.includes(specialities: { faculty: :college })
+
+    courses = courses.where(colleges: { id: params[:college_id] }) if params[:college_id]
+    courses = courses.where(faculties: { id: params[:faculty_id] }) if params[:faculty_id]
+    courses = courses.where(specialities: { id: params[:speciality_id] }) if params[:speciality_id]
+    courses = courses.where(semester: params[:semester]) if params[:semester]
+
+    render json: courses
   end
 
   def show
